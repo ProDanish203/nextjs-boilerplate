7591d78efcd38dd3fc3cd610570512eff00acab1
Danish <danishsidd524@gmail.com>
2024-05-11 13:41:07 +0500

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { AuthProvider } from "@/store/AuthProvider";
+import ReactQueryProvider from "@/store/ReactQueryProvider";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ReactQueryProvider>
+          <AuthProvider>
+            <Toaster position="top-right" />
+            <main className="">{children}</main>
+          </AuthProvider>
+        </ReactQueryProvider>
+      </body>
     </html>
   );
 }
